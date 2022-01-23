a81e90c452a8db5181323382b8ffa79148cb3984
balfron <40870085+balfron@users.noreply.github.com>
2022-01-23 12:26:49 +0000

    notify user test contract on rinkbey

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import { ChakraProvider, useDisclosure, Text } from "@chakra-ui/react";
 import { Layout } from "../layout";
 import { ConnectButton } from "../connect-button";
 import { AccountModal } from "../account-modal";
 import { Count } from "../count";
+import { useEthers } from "@usedapp/core";
 
 export const App: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { chainId } = useEthers();
+  console.log(chainId);
   return (
     <ChakraProvider>
       <Layout>
         <ConnectButton handleOpenModal={onOpen} />
         <AccountModal isOpen={isOpen} onClose={onClose} />
-        <Count />
+        {chainId !== 4 ? (
+          <Text color="white" fontSize="1xl" style={{ marginTop: 20 }}>
+            Connect to Rinkeby to see test contract
+          </Text>
+        ) : (
+          <Count />
+        )}
       </Layout>
     </ChakraProvider>
   );
